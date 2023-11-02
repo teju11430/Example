33b5790641faa3504b52da9da7290d663c5f2d62
teju11430 <mbteju411@gmail.com>
2023-11-02 14:47:56 +0530

    commiting

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const hasuraRouter = require("./hasura");
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Use the hasura route for handling data insertion
+app.use("/hasura", hasuraRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
---
+++ hasura.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const router = express.Router();
+
+router.post("/insert-signin", async (req, res) => {
+  try {
+    const email = req.body.email;
+    const phone = req.body.phone;
+    const password = req.body.password;
+
+    const mutation = `
+      mutation InsertSignin($email: String!, $phone: Int!, $password: String!) {
+        insert_signin(objects: { email: $email, phone: $phone, password: $password }) {
+          affected_rows
+        }
+      }
+    `;
+
+    const apiUrl = "https://pro-goose-67.hasura.app/v1/graphql";
+    const adminSecret =
+      "jboElHn7eyalm39pZ1n3vMpGk5egruSsHUuXVa4M0VIRUavnb4jzc6c2OslBGWwv";
+
+    const {default:fetch} = await import("node-fetch");
+
+    const response =await ( await fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "x-hasura-admin-secret": adminSecret,
+      },
+      body: JSON.stringify({
+        query: mutation,
+        variables: { email, phone, password },
+      }),
+    })).json;
+
+    const responseData = response;
+
+    if (responseData.errors) {
+      return res
+        .status(400)
+        .json({ error: "Failed to insert data into the table" });
+    } else {
+      return res.status(200).json({ message: "Data inserted successfully" });
+    }
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while processing the request" });
+  }
+});
+
+module.exports = router;
